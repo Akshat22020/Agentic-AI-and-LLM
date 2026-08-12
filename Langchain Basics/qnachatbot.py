69f36d58596448edeb02882243ca6270ad4681fe
Akshat2 <raj.akshat2202@gmail.com>
2026-08-12 17:07:50 +0530

    Initial Commit

---
+++ Langchain Basics/qnachatbot.py
@@ -0,0 +1,8 @@
+"""
+Simple LangChain Streamlit app with Groq
+A beginner-friendly version focusing on core concepts
+
+"""
+import streamlit as st
+from langchain.chat_models import init_chat_model
+from langchain_groq import ChatGroq
